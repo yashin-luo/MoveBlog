28094529359f06c4816baff809fd57f7505e466e
oscfox <395248168@qq.com>
2014-01-20 19:12:47 +0800

    encode to utf-8

--- src/spider/BlogPageProcessor.java
+++ src/spider/BlogPageProcessor.java
@@ -42,7 +42,7 @@ public class BlogPageProcessor implements PageProcessor{
 	 */
 	@Override
     public void process(Page page) {
-        if(java.util.regex.Pattern.matches(blogFlag, url)){
+        if(url.contains(blogFlag)){
         	getPage(page);
         	page.putField("getlinks", false);
         } else {
